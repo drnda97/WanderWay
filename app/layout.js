e8880816ee5a1fbf65d681898ec1e94bc5564081
Drndarevic Milan <32871193+drnda97@users.noreply.github.com>
2024-08-06 22:06:22 +0200

    06082024 - v5 - init commit

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,6 @@
-import { Inter } from "next/font/google";
 import "@mantine/core/styles.css";
 import "./globals.css";
 import { createTheme, MantineProvider } from "@mantine/core";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -18,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     // <MantineProvider theme={theme}>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
     // </MantineProvider>
   );
