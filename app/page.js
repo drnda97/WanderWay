811fbfff4d0108a14c141352742604c3c4d426c5
Drndarevic Milan <32871193+drnda97@users.noreply.github.com>
2024-08-07 23:25:28 +0200

    07082024 - v1 - second deploy

--- app/page.js
+++ app/page.js
@@ -1,14 +1,16 @@
+"use client";
 import styles from "./page.module.css";
 import React from "react";
 import logo from "./assets/images/homepage/logo.png";
+import { Field, Formik } from "formik";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.background__slider}></div>
       <div className={styles.main__content__wrapper}>
-        <img src={logo.src} alt="Logo" />
-        <h1 className={styles.page__title}>WanderWay</h1>
+        <img src={logo.src} alt="Logo" className={styles.logo} />
+        <h1 className={styles.page__title}>Hike&Mate</h1>
         <div className={styles.text__wrapper}>
           <h2 className={styles.page__subtitle}>
             Pripremite se za avanturu: prijavite se za našu novu aplikaciju i
@@ -21,6 +23,66 @@ export default function Home() {
             pratilac za otkrivanje novih staza, praćenje vaših planinarenja i
             povezivanje sa kolegama entuzijastima prirode.
           </p>
+        </div>
+        <div className={styles.form}>
+          <Formik
+            initialValues={{ email: "" }}
+            validate={(values) => {
+              const errors = {};
+              if (!values.email) {
+                errors.email = "Required";
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+              ) {
+                errors.email = "Invalid email address";
+              }
+              return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {({
+              values,
+              errors,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+              /* and other goodies */
+            }) => (
+              <form onSubmit={handleSubmit} className={styles.form__wrapper}>
+                <input
+                  className={`${styles.input} ${errors.email ? styles.input__error : ""}`}
+                  type="email"
+                  name="email"
+                  placeholder={"Unesi svoj mail"}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                />
+                {errors.email && <p className={styles.error}>{errors.email}</p>}
+                <label className={styles.checkbox}>
+                  <Field
+                    type="checkbox"
+                    name="agree"
+                    className={`${styles.input__checkbox} ${errors.email ? styles.input__error : ""}`}
+                  />
+                  Saglasan/na sam da primam obavestenja putem email adrese
+                </label>
+                <button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className={styles.green__button}
+                >
+                  Prijavi se
+                </button>
+              </form>
+            )}
+          </Formik>
         </div>
       </div>
     </main>
